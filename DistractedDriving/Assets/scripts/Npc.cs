c8da54ff28a535fe45754f255249745e5107560d
aariz-crtl-06 <aarizza75@gmail.com>
2025-10-30 11:05:03 -0400

    did some stuff.

--- DistractedDriving/Assets/scripts/Npc.cs
+++ DistractedDriving/Assets/scripts/Npc.cs
@@ -8,6 +8,7 @@ public class Npc : MonoBehaviour
     public Animator anim;
     public GameObject npc;
     CarManager car;
+    public GameObject granny;
 
     bool collided = false;
     void Start()
@@ -17,7 +18,7 @@ public class Npc : MonoBehaviour
 
     void Update()
     {
-        if(collided == true && car.horn==true)
+        if(car.canDrive == false && car.horn==true)
         {
             anim.enabled = false;
             car.canDrive = true;
@@ -28,7 +29,7 @@ public class Npc : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            
+            Debug.Log("Player collided with NPC");
             StartCoroutine(Npcs());
 
             collided = true;
@@ -38,8 +39,9 @@ public class Npc : MonoBehaviour
 
     private IEnumerator Npcs()
     {
+        granny.SetActive(true);
         anim.enabled = true;
-        Debug.Log("NPC collided with Player");
+        //Debug.Log("NPC collided with Player");
         yield return new WaitForSeconds(0.3f);
         car.currentSpeed = 0;
         car.canDrive = false;
